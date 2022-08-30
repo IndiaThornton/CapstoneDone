6d87eef12c054e00d79bef24d2bdd8a9d147c178
India Thornton <rthornton1314@gmail.com>
2022-08-30 12:09:21 -0400

    updates

--- server/index.js
+++ server/index.js
@@ -10,10 +10,7 @@ app.use(express.json())
 app.use(express.static('client'))
 app.use(cors())
 
-// const options={
-//      index:"home.html"
-// }
-// app.use("/",express.static("home",options))
+
 app.get('/', (req, res) => {
      res.sendFile(path.join(__dirname, '../client/home.html'))
  });
